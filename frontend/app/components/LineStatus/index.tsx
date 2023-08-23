616fe690f2372d58144d0d29a42f6f69e2d9606c
Felipe Borges <interpretadesign@gmail.com>
2023-08-23 14:03:13 -0300

    feat: status page

--- frontend/app/components/LineStatus/index.tsx
+++ frontend/app/components/LineStatus/index.tsx
@@ -13,14 +13,11 @@ function LineStatus({
 }) {
   return (
     <div className={styles["status-line-row"]}>
-      <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/icons/line.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles["status-line-icon"]}>
+        <svg width="6" height="30" viewBox="0 0 6 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M3.04327 23.8334C1.57051 23.8312 0.374831 25.0233 0.372646 26.4961C0.370461 27.9688 1.5626 29.1645 3.03535 29.1667C4.50811 29.1689 5.70379 27.9767 5.70597 26.504C5.70816 25.0312 4.51602 23.8355 3.04327 23.8334ZM2.53932 2.50492e-06L2.53931 26.5L3.53931 26.5L3.53932 2.92204e-06L2.53932 2.50492e-06Z" fill="#D7D7D7"/>
+        </svg>
+      </div>
       <section className={styles["line-name"]} style={style}>{name}</section>
       <section className={styles["status"]}>{status}</section>
     </div>
--- frontend/app/status/page.tsx
+++ frontend/app/status/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import LineStatus from '../components/LineStatus'
+import styles from "./Status.module.css"
 
 function Status() {
   return (
-    <div>
+    <div className={styles["status"]}>
+      <h4>Linhas de trem e metrô em São Paulo</h4>
       <LineStatus name='Linha-1 Azul' status='Status Normal' style={{backgroundColor: "blue"}} />
       <LineStatus name='Linha-1 Azul' status='Status Normal' style={{backgroundColor: "blue"}} />
       <LineStatus name='Linha-1 Azul' status='Status Normal' style={{backgroundColor: "blue"}} />
