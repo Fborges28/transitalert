262b5b7d760762b76431df0cc13ec3a4c3c69bdf
Felipe Borges <felipe@petloyalty.co>
2023-08-24 15:43:53 -0300

    feat: status updated_at

--- backend/src/Trains.js
+++ backend/src/Trains.js
@@ -1,5 +1,7 @@
 const cheerio = require("cheerio");
-
+const dayjs = require("dayjs");
+const customParserFormat = require("dayjs/plugin/customParseFormat");
+dayjs.extend(customParserFormat);
 const url = "https://www.viamobilidade.com.br/";
 
 async function getWebData() {
@@ -14,11 +16,19 @@ async function getWebData() {
         const title = $(element).find("span").attr("title");
         const color = $(element).find("span").css("background-color");
         const status = $(element).find(".status").text();
+        const locale = "pt-br";
+        const dateOption = {
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+        };
+        const updated_at = new Date().toLocaleDateString(locale, dateOption);
 
         const result = {
           title,
           status,
           color,
+          updated_at,
         };
 
         lines.push(result);
--- frontend/app/status/page.tsx
+++ frontend/app/status/page.tsx
@@ -3,34 +3,60 @@ import React, { useEffect } from 'react'
 import LineStatus from '../components/LineStatus'
 import styles from "./Status.module.css"
 import axios from 'axios'
+import Loading from '../components/Loading'
 
 interface Line {
   title: string,
   status: string,
-  color: string
+  color: string,
+  updated_at: string
 }
 
 function Status() {
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [error, setError] = React.useState<string>('');
   const [lines, setLines] = React.useState<Line[]>([]);
 
   useEffect(() => {
     axios.get<Line[]>('/trains/status')
       .then(response => {
+        setLoading(false);
         setLines(response.data);
       })
       .catch(error => {
         console.log(error)
+        setLoading(false);
+        setError(error.message)
       })
   }, [])
+
+  if(error) {
+    return (
+      <div className={styles["status"]}>
+        <h4>Erro no servidor</h4>
+      </div>
+    )
+  }
 
   return (
     <div className={styles["status"]}>
       <h4>Linhas de trem e metrô em São Paulo</h4>
       {
-        lines.map((line, index) => {
-          return <LineStatus key={index} name={line.title} status={line.status} style={{backgroundColor: line.color}} />
-        })
+        lines.length > 0 ? (
+          <p>Atualizado em {lines[0].updated_at}</p>
+        ): ""
       }
+      <div className={styles["status-container"]}>
+        {
+          loading ? (
+            <Loading />
+          ) : (
+            lines.map((line, index) => {
+              return <LineStatus key={index} name={line.title} status={line.status} style={{backgroundColor: line.color}} />
+            })
+          )
+        }
+      </div>
     </div>
   )
 }
